8ea07ede93e32f246bf34f10ee3dade0e822080f
Alex Morales <44649358+msalemor@users.noreply.github.com>
2024-07-07 13:32:42 -0400

    Create release.go

---
+++ cobra-viper-cli/cmd/release.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var releases = `cbcli v0.0.0 (yyyy-mm-dd):
+  - Initial release
+`
+
+func init() {
+	rootCmd.AddCommand(releasesCmd)
+}
+
+var releasesCmd = &cobra.Command{
+	Use:   "releases",
+	Short: "Prints the release information",
+	Long:  `Prints the release information`,
+	Run: func(cmd *cobra.Command, args []string) {
+    // print release information
+		printVersion()
+		fmt.Println(releases)
+	},
+}
