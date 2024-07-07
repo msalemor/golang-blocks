e2c54aeb33b9cba09523097780b085f5a9875813
Alex Morales <44649358+msalemor@users.noreply.github.com>
2024-07-07 13:19:39 -0400

    Update main.go

--- gin-static-basic-docker/main.go
+++ gin-static-basic-docker/main.go
@@ -9,18 +9,28 @@ import (
 )
 
 func main() {
+	// Create a router
 	r := gin.Default()
+	// Use CORS
 	r.Use(cors.Default())
 
+	// Create a route group
 	g := r.Group("/api/v1")
-  g.GET("/ping", func(c *gin.Context) {
+	
+	// Greate a GET PING endpoint
+	g.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
+	// Create a completion POST endpoint
 	g.POST("/completion", func(c *gin.Context) {})
+	// Create a rag POST endpoint
 	g.POST("/rag", func(c *gin.Context) {})
 
+	// Enable static files
 	r.Use(spa.Middleware("/", "./public"))
+	
+	// Serve
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
